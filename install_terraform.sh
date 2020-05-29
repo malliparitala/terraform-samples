57af4ddbb1fd4eb5bb62966d4050a8b317e0af14
Malli Paritala <malliparitala@gmail.com>
2020-05-29 03:39:32 +0000

    added variables.tf file

--- install_terraform.sh
+++ install_terraform.sh
@@ -3,3 +3,4 @@ wget https://releases.hashicorp.com/terraform/0.12.25/terraform_0.12.25_linux_am
 unzip -o terraform*.zip
 rm terraform*.zip
 echo "exec bash"
+cp ./getting_started/.credentials /home/gitpod/.aws/credentials
