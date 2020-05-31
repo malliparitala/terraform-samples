bbbe0515aacc4b8c8fb19aa12406adbef90228ea
Malli Paritala <malliparitala@gmail.com>
2020-05-31 17:20:24 +0000

    Updated vpc_example.tf

--- vpc/vpc_example.tf
+++ vpc/vpc_example.tf
@@ -12,9 +12,14 @@ resource "aws_vpc" "vpc_example" {
 	
 }
 
+data "aws_availability_zones" "available" {
+	state = "available"
+}
+
 resource "aws_subnet" "subnets_example" {
 	vpc_id = aws_vpc.vpc_example.id
 	cidr_block = "10.0.1.0/28"
+	availability_zone = data.aws_availability_zones.available.id
 
 	tags = {
 		Name = "Subnets"
