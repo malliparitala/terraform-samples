5ea526e5a0618b135a0ecaac041a17ad1f92a9f7
Malli Paritala <malliparitala@gmail.com>
2020-09-29 02:31:30 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,6 @@ terraform {
       name = "terraform-samples"
     }
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
 }
 
 # Configure the AWS Provider
