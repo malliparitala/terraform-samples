e0f813feb1b6647b16944c1f8035366b3253ddc9
Malli Paritala <malliparitala@gmail.com>
2020-12-11 03:34:21 +0000

    Added VPC Network Configuration

--- vpc.tf
+++ vpc.tf
@@ -29,6 +29,10 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 }
