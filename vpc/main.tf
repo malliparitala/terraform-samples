a0faef74a9f4d58ebc8d37f721eff3f051697c26
Malli Paritala <malliparitala@gmail.com>
2020-12-13 03:03:47 +0000

    Created vpc as a module

---
+++ vpc/main.tf
@@ -0,0 +1,29 @@
+# The configuration for the `remote` backend.
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "naga-paritala"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "terraform-samples"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.main_region
+}
