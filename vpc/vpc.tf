4eebc8c812a664eedb0348d47d0d2039efef8ad1
Malli Paritala <malliparitala@gmail.com>
2020-12-13 02:54:52 +0000

    Created vpc as a module

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,7 +1,7 @@
 resource "main_vpc" "main_vpc" {
-    cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/24"
 }
 
 resource "igateway" "main_internet_gateway" {
-    vpc_id = main_vpc.main_vpc.id
+  vpc_id = main_vpc.main_vpc.id
 }
