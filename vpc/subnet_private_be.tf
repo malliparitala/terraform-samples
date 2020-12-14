ec3cb3af1a119f0fc9eb79b8bf73b128f1147298
Malli Paritala <malliparitala@gmail.com>
2020-12-14 22:19:59 +0000

    Creating subnets in diff AZs

--- vpc/subnet_private_be.tf
+++ vpc/subnet_private_be.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "private_be" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.100.0/24"
+  availability_zone = "us-east-1b"
   depends_on = [aws_internet_gateway.main_internet_gateway]
 
   tags = {
--- vpc/subnet_private_db.tf
+++ vpc/subnet_private_db.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "private_db" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.200.0/24"
+  availability_zone = "us-east-1c"
   depends_on = [aws_internet_gateway.main_internet_gateway]
 
   tags = {
