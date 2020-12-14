7b4af647e5cb95c5e2d89938bef65117738a294a
Malli Paritala <malliparitala@gmail.com>
2020-12-14 15:07:46 +0000

    Passing subnet ids to eks module

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "main_region" {
   default = "us-east-1"
 }
+
+variable "subnet_fe_id" {
+  default = module.vpc.subnet_fe_id
+}
+
+variable "subnet_be_id" {
+  default = module.vpc.subnet_be_id
+}
+
+variable "subnet_db_id" {
+  default = module.vpc.subnet_db_id
+}
