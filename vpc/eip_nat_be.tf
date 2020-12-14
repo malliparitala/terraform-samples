98a8a3ab33436c911efdb04888d1135f4e525eaf
Malli Paritala <malliparitala@gmail.com>
2020-12-14 00:11:48 +0000

    seperated eips and nat gws as individual resources

--- vpc/eip_nat_be.tf
+++ vpc/eip_nat_be.tf
@@ -3,8 +3,3 @@ resource "aws_eip" "nat_be" {
   depends_on = [aws_internet_gateway.main_internet_gateway]
 }
 
-resource "aws_eip" "nat_db" {
-  vpc      = true
-  depends_on = [aws_internet_gateway.main_internet_gateway]
-}
-
