d64119ba49e58a9fee4c43df004e5a6252921301
Malli Paritala <malliparitala@gmail.com>
2020-12-14 15:23:36 +0000

    Passing subnet ids to eks module

---
+++ eks/variables.tf
@@ -0,0 +1,3 @@
+variable "subnet_fe_id" {}
+variable "subnet_be_id" {}
+variable "subnet_db_id" {}
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,7 +1,3 @@
 variable "cidr_block" {
   default = "10.0.0.0/16"
 }
-
-variable "subnet_fe_id" {}
-variable "subnet_be_id" {}
-variable "subnet_db_id" {}
