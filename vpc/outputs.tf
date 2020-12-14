a983348c9fc239cfed557986ed9d2a652caaa8e3
Malli Paritala <malliparitala@gmail.com>
2020-12-14 15:00:45 +0000

    Included eks role in eks module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
+
+output "subnet_fe_id" {
+  value = aws_subnet.public_fe.id
+}
+
+output "subnet_be_id" {
+  value = aws_subnet.private_be.id
+}
+
+output "subnet_db_id" {
+  value = aws_subnet.private_db.id
+}
