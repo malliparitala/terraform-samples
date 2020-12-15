a52b8df62ed40364ccd2cea5acc2cd98ec3efe21
Malli Paritala <malliparitala@gmail.com>
2020-12-15 02:36:43 +0000

    added endpoint to outputs

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,3 +1,11 @@
 output "eks_role_arn" {
   value = aws_iam_role.eks_role.arn
 }
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
