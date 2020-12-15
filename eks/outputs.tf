950e50ff0fa93349d0a4d73fce63ba671ba086bb
Malli Paritala <malliparitala@gmail.com>
2020-12-15 22:48:15 +0000

    Added eks node group

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -9,3 +9,11 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
+
+output "node_group_resources" {
+  value = aws_eks_node_group.eks_node_group.resources
+}
+
+output "node_group_status" {
+  value = aws_eks_node_group.eks_node_group.status
+}
